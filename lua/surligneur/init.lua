cf9313fbfc553cd7d4ca7a194104156bfd16dabe
Florian Martin <48833502+florianfmmartin@users.noreply.github.com>
2024-04-11 13:40:14 -0400

    maj color

--- lua/surligneur/init.lua
+++ lua/surligneur/init.lua
@@ -23,26 +23,26 @@ function M.load()
 		vim.api.nvim_set_hl(0, name, val)
 	end
 
-  vim.g.terminal_color_0 = colors.bg0
-  vim.g.terminal_color_8 = colors.bg0
+  vim.g.terminal_color_0 = colors.base0
+  vim.g.terminal_color_8 = colors.base1
 
-  vim.g.terminal_color_7 = colors.fg4
-  vim.g.terminal_color_15 = colors.fg4
+  vim.g.terminal_color_7 = colors.base6
+  vim.g.terminal_color_15 = colors.base7
 
   vim.g.terminal_color_1 = colors.red
-  vim.g.terminal_color_9 = colors.light_red
+  vim.g.terminal_color_9 = colors.pink
 
-  vim.g.terminal_color_2 = colors.grren
+  vim.g.terminal_color_2 = colors.green
   vim.g.terminal_color_10 = colors.light_green
 
   vim.g.terminal_color_3 = colors.yellow
-  vim.g.terminal_color_11 = colors.light_yellow
+  vim.g.terminal_color_11 = colors.orange
 
   vim.g.terminal_color_4 = colors.blue
   vim.g.terminal_color_12 = colors.light_blue
 
-  vim.g.terminal_color_5 = colors.magenta
-  vim.g.terminal_color_13 = colors.light_magenta
+  vim.g.terminal_color_5 = colors.purple
+  vim.g.terminal_color_13 = colors.light_purple
 
   vim.g.terminal_color_6 = colors.cyan
   vim.g.terminal_color_14 = colors.light_cyan
